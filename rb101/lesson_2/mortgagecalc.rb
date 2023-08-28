7994a7bbfa235a44d5fa40dfee5a34b067c1e76e
Jung-ah Han <127273499+4coder23@users.noreply.github.com>
2023-08-27 22:37:33 -0400

    mortgage calculator

---
+++ rb101/lesson_2/mortgagecalc.rb
@@ -0,0 +1,76 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgagecalc_messages.yml')
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def valid_number?(number)
+  (number.to_i.to_s == number || number.to_f.to_s == number) &&
+    (number != '0' && number != '0.0') && number * 1 > '0'
+end
+
+def prompt_loan_amount
+  loop do
+    loan_amount = Kernel.gets.chomp
+
+    if valid_number?(loan_amount)
+      return loan_amount
+    else
+      prompt(MESSAGES['invalid_loanamount'])
+    end
+  end
+end
+
+def prompt_apr
+  loop do
+    apr = Kernel.gets.chomp
+
+    if valid_number?(apr)
+      return apr
+    else
+      prompt(MESSAGES['invalid_apr'])
+    end
+  end
+end
+
+def prompt_loan_length
+  loop do
+    loan_length = Kernel.gets.chomp
+
+    if valid_number?(loan_length)
+      return loan_length
+    else
+      prompt(MESSAGES['invalid_loanlength'])
+    end
+  end
+end
+
+prompt(MESSAGES['welcome'])
+
+loop do # main loop
+  prompt(MESSAGES['loan_amount'])
+  loan_amount = prompt_loan_amount
+  prompt("Your loan amount is $#{loan_amount}.")
+
+  prompt(MESSAGES['apr'])
+  apr = prompt_apr
+  prompt("Your APR is #{apr}%.")
+
+  prompt(MESSAGES['loan_length'])
+  loan_length = prompt_loan_length
+  prompt("Your loan duration is #{loan_length} in months.")
+
+  monthly_interest = apr.to_f / 100 / 12
+  monthly_payment = loan_amount.to_i *
+                    (monthly_interest /
+                    (1 - ((1 + monthly_interest)**(-loan_length.to_i))))
+  prompt("Your monthly mortgage payment is: $#{monthly_payment.round(2)}.")
+
+  prompt(MESSAGES['recalc'])
+  recalc = Kernel.gets.chomp
+  if recalc.downcase.include?('n')
+    prompt(MESSAGES['bye'])
+    break
+  end
+end
