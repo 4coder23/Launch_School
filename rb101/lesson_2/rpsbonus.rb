3ff78226f47feb99178bca669cb57c7ac48a17d8
Jung-ah Han <127273499+4coder23@users.noreply.github.com>
2023-09-24 18:12:11 -0400

    Refactored RPS bonus

--- rb101/lesson_2/rpsbonus.rb
+++ rb101/lesson_2/rpsbonus.rb
@@ -1,9 +1,8 @@
 system('clear')
 require 'yaml'
 MESSAGES = YAML.load_file('rpsbonus_messages.yml')
-VALID_CHOICES = %w(r p s sp l)
-
-choice = ''
+VALID_CHOICES = %w(r rock p paper s scissors sp spock l lizard)
+VALID_REPLAY = %w(y yes n no)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
@@ -18,7 +17,7 @@ def get_name
       prompt(MESSAGES['valid_name'])
     else
       prompt("Hi #{name}!  Win three times and you'll be the grand winner!")
-      break
+      return name
     end
   end
 end
@@ -26,72 +25,117 @@ end
 def get_choice
   prompt(MESSAGES['game_rule'])
   loop do
-    choice = Kernel.gets().chomp()
+    user_choice = Kernel.gets().chomp().downcase()
 
-    if VALID_CHOICES.include?(choice)
-      return choice
+    if VALID_CHOICES.include?(user_choice)
+      return user_choice
     else
       prompt(MESSAGES['valid_choice'])
     end
   end
 end
 
-win = { 'r' => ['s', 'l'],
-        's' => ['p', 'l'],
-        'p' => ['r', 'sp'],
-        'l' => ['sp', 'p'],
-        'sp' => ['s', 'r'] }
+WIN_FORMULA = { 'r' => ['s', 'scissors', 'l', 'lizard'],
+                'rock' => ['s', 'scissors', 'l', 'lizard'],
+                's' => ['p', 'paper', 'l', 'lizard'],
+                'scissors' => ['p', 'paper', 'l', 'lizard'],
+                'p' => ['r', 'rock', 'sp', 'spock'],
+                'paper' => ['r', 'rock', 'sp', 'spock'],
+                'l' => ['sp', 'spock', 'p', 'paper'],
+                'lizard' => ['sp', 'spock', 'p', 'paper'],
+                'sp' => ['s', 'scissors', 'r', 'rock'],
+                'spock' => ['s', 'scissors', 'r', 'rock'] }
 
-choice_to_message = { 'r' => 'rock',
+CHOICE_TO_MESSAGE = { 'r' => 'rock',
+                      'rock' => 'rock',
                       'p' => 'paper',
+                      'paper' => 'paper',
                       's' => 'scissors',
+                      'scissors' => 'scissors',
                       'sp' => 'spock',
-                      'l' => 'lizard' }
+                      'spock' => 'spock',
+                      'l' => 'lizard',
+                      'lizard' => 'lizard' }
+
+def determine_winner(user_choice, computer_choice)
+  if WIN_FORMULA.fetch(user_choice).include?(computer_choice)
+    "You"
+  elsif WIN_FORMULA.fetch(computer_choice).include?(user_choice)
+    "Computer"
+  end
+end
+
+def display_winner(winner)
+  if winner == "You"
+    prompt('You win!')
+  elsif winner == "Computer"
+    prompt('Computer wins!')
+  else
+    prompt("It's a tie!")
+  end
+end
+
+scoreboard = { 'You' => 0,
+               'Computer' => 0 }
+
+def increment_score(winner, scoreboard)
+  if winner == "You"
+    scoreboard['You'] += 1
+  elsif winner == "Computer"
+    scoreboard['Computer'] += 1
+  end
+  prompt("Your score: #{scoreboard['You']} Computer score: #{scoreboard['Computer']}")
+end
+
+def get_replay_choice
+  prompt(MESSAGES['replay'])
+  loop do
+    replay_choice = Kernel.gets().chomp().downcase()
+    if VALID_REPLAY.include?(replay_choice)
+      return replay_choice
+    else
+      prompt(MESSAGES['valid_replay_choice'])
+    end
+  end
+end
+
+def set_replay(replay_choice, scoreboard)
+  puts
+  if replay_choice.start_with?('y')
+    scoreboard["You"] = 0
+    scoreboard["Computer"] = 0
+    system('clear')
+  end
+end
 
 prompt(MESSAGES['greeting'])
-get_name
+name = get_name
 puts
 
 loop do # main loop
-  your_score = 0
-  computer_score = 0
   loop do
-    choice = get_choice
+    user_choice = get_choice
     computer_choice = VALID_CHOICES.sample
-    puts
-    prompt("You chose: #{choice_to_message[choice]} Computer chose: #{choice_to_message[computer_choice]}")
+    system('clear')
+    prompt("You chose: #{CHOICE_TO_MESSAGE[user_choice]} Computer chose: #{CHOICE_TO_MESSAGE[computer_choice]}")
 
-    if win.fetch(choice).include?(computer_choice)
-      prompt('You win!')
-      your_score += 1
-    elsif win.fetch(computer_choice).include?(choice)
-      prompt('Computer wins!')
-      computer_score += 1
-    else
-      prompt("It's a tie!")
-      (your_score + 0) && (computer_score + 0)
+    winner = determine_winner(user_choice, computer_choice)
+    display_winner(winner)
+    increment_score(winner, scoreboard)
+    if scoreboard["You"] == 3
+      prompt(MESSAGES['you_win'])
+      break
+    elsif scoreboard["Computer"] == 3
+      prompt(MESSAGES['computer_win'])
+      break
     end
-
-    prompt("Your score: #{your_score} Computer score: #{computer_score}")
-    break if your_score == 3 || computer_score == 3
     puts
   end
 
-  if your_score == 3
-    prompt(MESSAGES['you_win'])
-  else
-    prompt(MESSAGES['computer_win'])
-  end
-  puts
-
-  prompt(MESSAGES['replay'])
-  replay = Kernel.gets().chomp()
-  puts
-  if replay.downcase.start_with?('y')
-    your_score = 0
-    computer_score = 0
-  else
-    prompt(MESSAGES['goodbye'])
+  replay_choice = get_replay_choice
+  set_replay(replay_choice, scoreboard)
+  if replay_choice == 'n' || replay_choice == 'no'
+    prompt("#{name}, thank you for playing. Good bye!")
     break
   end
 end
